6b70ba4622fb08a3d756aa43703522702ffd9f74
Douglas Campbell <dacampbelltbone@gmail.com>
2019-08-23 21:39:06 -0400

    Added storage of logged in user sessions and cookies. MVP complete.

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-const userRouter = require("./users/user-router");
+const usersRouter = require("./users/user-router");
+const authRouter = require("./auth/auth-router");
+
+const sessionOptions = {
+  name: "acookie",
+  secret: "wynonabigbadbeaver",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+
+  store: new knexSessionStore({
+    knex: require("./data/db-config"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
 
 const server = express();
 
@@ -10,6 +33,9 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use("/api", userRouter);
+server.use(session(sessionOptions));
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 module.exports = server;
